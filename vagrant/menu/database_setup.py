b0ca8fb596e889b1264a8bfb742c5e08c4921638
Alexander Frolov <big_air2409@mail.ru>
2018-06-23 14:15:45 +1200

    Added database ORM - Object Relation Mapper Works OK

---
+++ vagrant/menu/database_setup.py
@@ -0,0 +1,45 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+# Because I use Vagrant from Udacity SQLAlchemy is 
+# already installed on Virtual Machine(VM)
+# Base is a declarative Data Base
+Base = declarative_base()
+
+########
+class Restaurant(Base):
+
+    __tablename__ = 'restaurant'
+
+    # Mapper
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+
+########
+class MenuItem(Base):
+
+    __tablename__ = 'menu_item'
+
+    # Mapper
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    # restaurant_id is a foreign key
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restauratn_id = relationship(Restaurant)
+
+
+
+
+
+
+########## Insert at the end of the file ##########
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+# Adds classes as new tables in our DB
+Base.metadata.create_all(engine)
