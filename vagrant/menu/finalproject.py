bed7330ed663d8137b2bc718e7768a88106aa817
Alexander Frolov <big_air2409@mail.ru>
2018-07-03 13:24:10 +1200

    Added edit, delete, add restaurant. The app is fully functional. Need to add styling and JSON endpoints. Works OK.

--- vagrant/menu/finalproject.py
+++ vagrant/menu/finalproject.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 ################ Create Connection to DB ################
 # init connection with DB
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
 
 # Connection between class def and corresp table in DB
 Base.metadata.bind = engine
@@ -24,7 +24,9 @@ session = DBSession()
 ################ Getters/Setters ################
 
 def getRestaurant(restaurantID):
-    return session.query(Restaurant).filter_by(id = restaurantID).one()
+    restaurant = session.query(Restaurant).filter_by(id = restaurantID).one()
+    print("L28 restaurant = " + restaurant.name + " ##########")
+    return restaurant
 
 def getMenuItems(restaurantID):
     return session.query(MenuItem).filter_by(restaurant_id = restaurantID).all()
@@ -40,31 +42,57 @@ def getMenuItem(restaurantID, menuItemID):
 
 # Show all restaurants
 @app.route('/')
-@app.route('/restaurants/')
+@app.route('/restaurant/')
 def showRestaurants():
     restaurants = session.query(Restaurant).all()
     return render_template('restaurants.html', restaurants = restaurants)
 
 # Create new restaurant
 @app.route('/restaurant/new/', methods=['GET', 'POST'])
-def newRestaurant(restaurantID):
-    return render_template('newrestaurant.html')
+def newRestaurant():
+    if request.method == 'POST':
+        if request.form['name']:
+            restaurant = Restaurant(name = request.form['name'], )
+            session.add(restaurant)
+            session.commit()
+            flash("Restaurant: " + restaurant.name + " added.")
+            return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('newrestaurant.html')
 
 # Edit restaurant
 @app.route('/restaurant/<int:restaurantID>/edit/', methods=['GET', 'POST'])
 def editRestaurant(restaurantID):
-    return render_template('editrestaurant.html')
+    # TODO: Add redirect when successful
+    restaurant = getRestaurant(restaurantID)
+    if request.method == 'POST':
+        if request.form['name']:
+            restaurant.name = request.form['name']
+            message = "New Restaurant name is " + request.form['name']
+            session.add(restaurant)
+            session.commit()
+            flash(message)
+            return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('editrestaurant.html', restaurant = restaurant)
 
 # Delete restaurant 
 @app.route('/restaurant/<int:restaurantID>/delete/', methods=['GET', 'POST'])
 def deleteRestaurant(restaurantID):
-    return render_template('deleterestaurant.html')
+    restaurant = getRestaurant(restaurantID)
+    if request.method == 'POST':
+        session.delete(restaurant)
+        session.commit()
+        flash("Restaurant " + restaurant.name + " was deleted")
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('deleterestaurant.html', restaurant=restaurant)
 
 ################ Routs ################
 ################ MenuItem ################
 # Show restaurant menu items
 @app.route('/restaurant/<int:restaurantID>/menu/')
-def showMenuItems(restaurantID):
+def restaurantMenu(restaurantID):
     print("############### Show restaurant menu ###############")
     restaurant = getRestaurant(restaurantID)
     print("Restaurant = %s" % restaurant.name)
